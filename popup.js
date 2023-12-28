b8e5e51358f8f4fc69d8554d92e495ac7f0e2997
Hardique Dasore <37050252+hardiquedasore@users.noreply.github.com>
2023-12-28 20:43:21 +1100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,33 @@
+const checkPayPal = () => {
+  const dom = [...document.querySelectorAll('script')];
+  var pay = null
+  dom.forEach((el)=>{
+    if(el?.innerHTML.toLowerCase().includes('paypal')){
+      pay = el
+    }
+   
+  })
+ 
+  return pay;
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
+  
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: checkPayPal
+      }, (result) => {
+        if(result && result[0].result !==null){
+          document.getElementById("PayPal").innerHTML = "Yes"
+        }
+        else{
+          document.getElementById("PayPal").innerHTML = "No"
+        }
+      });
       var currentTab = tabs[0];
-      if (currentTab.url.startsWith('https://')) {
+          if (currentTab.url.startsWith('https://')) {
         document.getElementById("SSL").innerHTML = "Yes"
       } else {
         document.getElementById("SSL").innerHTML = "No"
